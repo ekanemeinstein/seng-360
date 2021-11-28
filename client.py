26e2f19af480d9e650d332faa0749c220016c629
ekanemeinstein <ekyeinstein2012@gmail.com>
2021-11-28 11:35:45 -0800

    -- images update --

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket, select
 from sys import stdin, stdout
 from _thread import *
 from os import system
+from PIL import Image
 
 # Create stream socket and connect to server IP
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -22,20 +23,63 @@ dest_user = stdin.readline()
 
 _ = system('clear')
 print(f"Starting chat with {dest_user}\n\n")
+# Incoming messages/images
 def handle_incoming(client_socket):
     while True:
         try:
-            server_message = client_socket.recv(2048)
-            print(server_message.decode())
+            server_message = client_socket.recv(5000)
+            text = server_message.decode()
+            if text.startswith("#"): receive_image(text[1:], client_socket)
+            else: print(text)
         except:
             continue
 
+def receive_image(name, socket):
+    output = f"{name[:-4]}_out.{name[-3:]}"
+    f = open(output,'wb')
+    print ("-----Receiving image-----")
+    image = socket.recv(5000)
+    bytes_rcvd = len(image)
+    while (bytes_rcvd>=5000):
+        #print ("Receiving image...")
+        f.write(image)
+        image = socket.recv(5000)
+        bytes_rcvd = len(image)
+        #print(f"{bytes_rcvd} bytes sent")
+    f.write(image)    
+    f.close()
+    print (f"-----Image saved as {output}-----")
+    with open(output, 'rb') as f:
+        im = Image.open(f)
+        im.show()
+
+# Send Image
+def image_handler(image):
+    f = open(image,'rb')
+    print(f"-----uploading {image}-----")
+    message = f.read(5000)
+    while (message):
+        client_socket.send(message)
+        message = f.read(5000)
+    f.close()
+    print (f"-----uploaded {image}-----")     
+
+# Handle incoming messages as separate thread
 start_new_thread(handle_incoming, (client_socket,))        
+
+# Client commands/outgoing messages
 while True:
     chat = stdin.readline()
+    # Abort session
     if (chat[:4] == 'exit'):
         print("Logging out...")
         break
+    # Upload Image
+    elif (chat.startswith("#")):
+        message = f"{chat.strip()}:{dest_user.strip()}"
+        client_socket.send(message.encode())        
+        image_handler(chat[1:].strip())
+    # Send Message
     else:
         message = f"@{dest_user.strip()}: {chat.strip()}"
         client_socket.send(message.encode())
--- server.py
+++ server.py
@@ -14,12 +14,26 @@ server_socket.listen(5)
 print("Server socket created and listening for connections on localhost:10000\n")
 print("listening...\n")
 
+def image_handler(dest, connection):
+    image = connection.recv(5000)
+    bytes_rcvd = len(image)
+    print(f"{bytes_rcvd} bytes sent")
+    while (bytes_rcvd>=5000):
+        print ("Receiving...")
+        dest.send(image)
+        image = connection.recv(5000)
+        bytes_rcvd = len(image)
+        print(f"{bytes_rcvd} bytes sent")
+    dest.send(image)
+    print ("Done Receiving")
+
 def client_handler(connection, client_address):
     while True:
             try:
-                message = connection.recv(2048)
+                message = connection.recv(5000)
                 text = message.decode()
                 print(f"{client_address}: {text}\n")
+                # Login
                 if text.startswith("^"):
                     # Extract username and password
                     client_name = text[1:text.index(':')].lower()
@@ -31,22 +45,29 @@ def client_handler(connection, client_address):
                     # Inform users that new user is online
                     broadcast((f"{client_name} is online.").encode())
                     print(f"{client_name} is online.")
+                # Messaging    
                 elif text.startswith("@"):
-                    dest_client_name = text[1:text.index(':')].lower()
-                    chat = text[1:]
-                    private(chat.encode(),users[dest_client_name]) 
+                    dest_client_name, chat = text[1:].split(":")
+                    m = f"{client_name}:{chat}"
+                    private(m.encode(), users[dest_client_name]) 
+                # Images
+                elif text.startswith("#"):
+                    name, dest_client_name = text.split(":")
+                    print(f"{client_name} wants to send image {name[1:]} to {dest_client_name}")
+                    private(name.encode(), users[dest_client_name])
+                    image_handler(users[dest_client_name], connection)
                 else:
                     connections.remove(connection)
             except:
                 continue
-
+# Direct Message
 def private(message, destination):
     try:
         destination.send(message)
     except:
         destination.close()
         connections.remove(destination)
-
+# Broadcast Message
 def broadcast(message, source):    
     for connection in connections:
         if connection!=source:
@@ -63,6 +84,7 @@ while in_session:
         connection.send(b"You are connected")
         print(f"{client_address} is connected.\n")
         
+        # Start each new client connection as a thread
         start_new_thread(client_handler,(connection,client_address))  
     except:
         in_session = False
