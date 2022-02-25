2d5faea98e453c33f1b90cac6521587b6a8232d2
samuel-barrett <90578414+samuel-barrett@users.noreply.github.com>
2022-02-24 18:08:20 -0800

    Fix syntax errors for build.bash

--- build.bash
+++ build.bash
@@ -1,38 +1,36 @@
-!#/bin/bash
+#!/bin/bash
 
-#Process command line arguments
-if [ $# -ne 1 ]; then
+#Process ensure on ly one or 2 command line arguments are present
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
     echo "Use: $0 --help for usage instructions"
     exit 1
 fi
 
-method = $1
-
-if [ $method = "--help" ]; then
+if [ $1 = "--help" ]; then
     echo "Usage: $0 [--help] [--run]"
     echo "--help: Prints this message"
     echo "--run [file]: Runs the server or client"
     exit 0
-elif [ $method = "--run" ]; then
+elif [ $1 = "--run" ]; then
     if [ $# -ne 2 ]; then
         echo "Use: $0 --help for usage instructions"
         exit 1
     fi
     file = $2
-    if [ $file = "server" ]; then
+    if [ $2 = "server" ]; then
         echo "Running server"
         python3 server.py &
-    elif [ $file = "client" ]; then
+    elif [ $2 = "client" ]; then
         echo "Running client"
         python3 client.py &
-    elif [ $file = "test" ]; then
+    elif [ $2 = "test" ]; then
         echo "Running tests script"
         bash test_run.bash
     else
         echo "Invalid file"
         exit 1
     fi
-elif [ $method = "--test" ]; then
+elif [ $1 = "--test" ]; then
     echo "Running tests script"
     bash test_run.bash
 else
